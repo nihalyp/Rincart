dbe2113d299b67e020238421bbc9a262bcd80f07
nihalyp <niiihalyp@gmail.com>
2026-07-12 13:59:00 +0530

    Update Cloudinary storage settings with credentials
    Replaced environment variables with hardcoded Cloudinary credentials.

--- rincart/settings.py
+++ rincart/settings.py
@@ -210,9 +210,9 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME':'iskzc8vd',
+    'API_KEY':'XZbX_Y00ZvC1jjlL1jCu1NEdlyo',
+    'API_SECRET': '962581395445832',
 }
 
 
