55c9ae24b279d6f1eb5417580a9856e5ea20bc14
nihalyp <niiihalyp@gmail.com>
2026-07-14 20:23:21 +0530

    Add staticfiles_urlpatterns for static file handling
    Import staticfiles_urlpatterns and update urlpatterns to include it for serving static files.

--- rincart/urls.py
+++ rincart/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # 👈 ഇത് ഇംപോർട്ട് ചെയ്യുക
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 ]
 
-# ഈ വരികൾ ഒരു കണ്ടീഷനും ഇല്ലാതെ നേരിട്ട് ഏറ്റവും താഴെ ചേർക്കുക:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# ഇത് രണ്ട് വരികളും കണ്ടീഷൻ ഇല്ലാതെ ഏറ്റവും താഴെ കൊടുക്കുക:
+urlpatterns += staticfiles_urlpatterns() # 👈 സ്റ്റാറ്റിക് ഫയലുകൾ വൈറ്റ്നോയിസ് വഴി റീഡ് ചെയ്യാൻ
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
