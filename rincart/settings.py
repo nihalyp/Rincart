3e8c79c5ac9de17c3fa16e9758f3bc14736473e8
nihalyp <niiihalyp@gmail.com>
2026-07-14 20:22:19 +0530

    Update static file storage configuration
    Removed STORAGES configuration and set STATICFILES_STORAGE.

--- rincart/settings.py
+++ rincart/settings.py
@@ -107,11 +107,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # Media Cloudinary configuration
